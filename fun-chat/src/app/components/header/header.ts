4c8dd514b7926581fbeb5af1d9cfadd1ba70766c
YulikK <yulikpoch@gmail.com>
2024-04-15 15:22:44 +0200

    feat: add logo refactor: navigate list

--- fun-chat/src/app/components/header/header.ts
+++ fun-chat/src/app/components/header/header.ts
@@ -27,6 +27,7 @@ export default class Header extends BaseComponent {
     super({ tag: 'header' });
     this.navList = this.getNavList();
     this.userInfo = div({ className: classes.wrap });
+
     this.navList.append(this.userInfo);
     this.append(this.navList);
   }
@@ -43,43 +44,48 @@ export default class Header extends BaseComponent {
     }
   }
 
-  public changeAuth(isAuth: boolean, user?: User): boolean {
-    let result = false;
+  public changeAuth(isAuth: boolean, user?: User): void {
     this.isAuth = isAuth;
-    if (this.isAuth) {
-      this.items.forEach(item => {
-        item.removeClass(classes.disable!);
-        const name = item.getElement().dataset.data;
-        if (name === 'auth') {
-          item.setText('Logout');
-        }
-      });
-      if (user) {
-        this.userInfo.appendChild([
-          img({ src: 'img/user.png', alt: 'user', className: classes.img, width: 24, height: 24 }),
-          p(classes.text!, user.login)
-        ]);
-      }
-      
-      result = true;
-    } else {
-      this.items.forEach(item => {
-        const name = item.getElement().dataset.data;
-        if (name && NEED_AUTH.includes(getNavigation(name))) {
-          item.getElement().classList.add(classes.disable!);
-        }
-        if (name === 'auth') {
-          item.setText('Logout');
-        }
-      });
-      this.userInfo.clear();
-    }
-    return result;
+    this.items.forEach(item => {
+      this.updateNavItem(item);
+      this.updateUserInfo(user);
+    });
   }
-  
+
+  private updateNavItem(item: BaseComponent): void {
+    item.removeClass(classes.disable!);
+    const { data } = item.getElement().dataset;
+    if (data) {
+      const tittle = this.getTittle(data);
+      item.setText(tittle);
+    }
+
+    if (!this.isAuth && data && NEED_AUTH.includes(getNavigation(data))) {
+      item.getElement().classList.add(classes.disable!);
+    }
+  }
+
+  private updateUserInfo(user?: User): void {
+    this.userInfo.clear();
+    if (this.isAuth && user) {
+      this.userInfo.appendChild([
+        img({ src: 'img/user.png', alt: 'user', className: classes.img, width: 24, height: 24 }),
+        p(classes.text!, user.login)
+      ]);
+    }
+  }
+
   private getNavList(): BaseComponent {
     const navMenu = nav({ className: classes.navbar });
     const navList = ul({ className: classes['navbar-nav'] });
+    this.updateItemsArray();
+    navList.appendChild(this.items);
+    const logo = img({ src: 'img/logo.png', alt: 'logo', className: classes.logo, width: 100, height: 35 });
+    navMenu.appendChild([logo, navList])
+    return navMenu;
+  }
+
+  private updateItemsArray(): void {
     Object.entries(Navigation).forEach(([key, value]) => {
       if (value && key) {
         const needsAuth = !this.isAuth && NEED_AUTH.includes(value);
@@ -87,9 +93,6 @@ export default class Header extends BaseComponent {
         this.items.push(newItems);
       }
     });
-    navList.appendChild(this.items);
-    navMenu.appendChild([navList])
-    return navMenu;
   }
 
   private getNavItem(tittle: string, data: string, active: boolean, disable: boolean): BaseComponent {
@@ -115,7 +118,7 @@ export default class Header extends BaseComponent {
           navigateTo(navPage);
           this.setActivePage(navPage);
         }
-        
+
       }
     }
   }
@@ -126,13 +129,13 @@ export default class Header extends BaseComponent {
     }
   }
 
-    private getTittle(name: string): string {
-      let tittle = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-      if (name === 'auth') {
-        tittle = this.isAuth ? 'Logout' : 'Login';
-      }
-      return tittle;
+  private getTittle(name: string): string {
+    let tittle = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    if (name === 'auth') {
+      tittle = this.isAuth ? 'Logout' : 'Login';
     }
+    return tittle;
+  }
 
 
 }
